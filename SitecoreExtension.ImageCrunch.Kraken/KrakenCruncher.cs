259fb29dfc25989340802993e55b378cd957db2f
Thor Jakobsen <dunston@hackthedot.dk>
2013-12-08 23:05:40 +0100

    added kraken.io cruncher

---
+++ SitecoreExtension.ImageCrunch.Kraken/KrakenCruncher.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Sitecore;
+using SitecoreExtension.ImageCrunch.Entities;
+using SitecoreExtension.ImageCrunch.Interfaces;
+
+namespace SitecoreExtension.ImageCrunch.Kraken
+{
+    public class KrakenCruncher : ICruncher
+    {
+        public CrunchResult CrunchStream(Stream stream)
+        {
+            var client = new HttpClient();
+            var content = new MultipartFormDataContent();
+
+            var list = new List<KeyValuePair<string, JsonValue>>();
+
+            JsonValue value = new JsonObject
+            {
+                {
+                    "auth", new JsonObject
+                    {
+                        {
+                            "api_key", Settings.ApiKey
+                        },
+                        {
+                            "api_secret", Settings.ApiSecret
+
+                        }
+                    }
+                    
+
+                },
+                {
+                    "wait", true
+                }
+            };
+
+            var stringContent = new StringContent(value.ToString());
+            
+            content.Add(stringContent, "woop");
+            var streamContent = new StreamContent(stream);
+            streamContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+            content.Add(streamContent, "file ", "hello .jpg");
+
+            HttpResponseMessage postAsync = client.PostAsync("https://api.kraken.io/v1/upload", content).Result;
+
+            Response jsonResult = null;
+            if (postAsync.IsSuccessStatusCode)
+            {
+                string stringResult = postAsync.Content.ReadAsStringAsync().Result;
+
+                var dynamicResut = JsonValue.Parse(stringResult);
+
+                if (!dynamicResut.ContainsKey("error"))
+                {
+                    jsonResult = postAsync.Content.ReadAsAsync<Response>().Result;
+                }
+                else
+                {
+                    throw new Exception(string.Format("Error: {0}", dynamicResut.GetValue("error").ReadAs<string>()));
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+            HttpResponseMessage httpResponseMessage = client.GetAsync(jsonResult.Dest, HttpCompletionOption.ResponseHeadersRead).Result;
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            Stream result = httpResponseMessage.Content.ReadAsStreamAsync().Result;
+
+            var smushItObject = new Entities.CrunchResult();
+            smushItObject.FileStream = result;
+            smushItObject.Format = Path.GetExtension(jsonResult.Dest);
+
+            return smushItObject;
+        }
+
+        public decimal MaxImageSize
+        {
+            get
+            {
+                return int.MaxValue;
+            }
+        }
+    }
+
+
+}
---
+++ SitecoreExtension.ImageCrunch.Kraken/Settings.cs
@@ -0,0 +1,50 @@
+﻿namespace SitecoreExtension.ImageCrunch.Kraken
+{
+    public static class Settings
+    {
+        private static string _apiKey;
+        private static string _apiSecret;
+        private static string _maxImageSize;
+
+        public static string ApiKey
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_apiKey))
+                {
+                    _apiKey =  Sitecore.Configuration.Settings.GetSetting("Kraken.ApiKey");
+
+                }
+                return _apiKey;
+            }
+        }
+
+        public static string ApiSecret
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_apiSecret))
+                {
+                    _apiSecret = Sitecore.Configuration.Settings.GetSetting("Kraken.ApiSecret");
+
+                }
+                return _apiSecret;
+            }
+            
+        }
+
+        public static string MaxImageSize
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_maxImageSize))
+                {
+                    _maxImageSize = Sitecore.Configuration.Settings.GetSetting("Kraken.MaxImageSize");
+
+                }
+                return _maxImageSize;
+            }
+
+        }
+    }
+}
